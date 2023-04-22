066272b809fe6d10ae6b05b765f02807853a118d
lucjdyoung <lucjdyoung@gmail.com>
2023-04-22 15:07:50 +0100

    start/initial setup

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -1,17 +1,12 @@
-import { component$ } from '@builder.io/qwik';
-import { useServerTimeLoader } from '~/routes/layout';
-import styles from './footer.module.css';
+import { component$ } from "@builder.io/qwik";
+import { useServerTimeLoader } from "~/routes/layout";
 
 export default component$(() => {
   const serverTime = useServerTimeLoader();
 
   return (
     <footer>
-      <a href="https://www.builder.io/" target="_blank" class={styles.anchor}>
-        Made with ♡ by Builder.io
-        <span class={styles.spacer}>|</span>
-        <span>{serverTime.value.date}</span>
-      </a>
+      <span>{serverTime.value.date}</span>
     </footer>
   );
 });
